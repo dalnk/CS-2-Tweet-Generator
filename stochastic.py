10110f94682bfc938ac46d5e5fb87c6c35edff58
Dennis Aleynikov <daleynikov@gmail.com>
2018-01-29 03:43:19 -0800

    patch for wrong frequency

--- stochastic.py
+++ stochastic.py
@@ -13,18 +13,24 @@ def main(argv):
 
 def sample(h):
     r = random.random()
-    f = convert_freq(h)
+    f = convert_freq(h, count_words(h))
     i = float(0)
     for w in f:
-        i = i + w[1]
+        i = i + float(w[1])
         if i > r:
             return w[0]
 
-def convert_freq(h):
+def convert_freq(h, count):
     freq = []
     for t in h:
-        freq.append((t[0], frequency.freq(h,t[0])))
+        freq.append((t[0], frequency.freq(h,t[0],count)))
     return freq
+
+def count_words(h):
+    words = 0
+    for n in h:
+        words = words + n[1]
+    return words
 
 
 if __name__ == "__main__":

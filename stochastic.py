32c5d55b7aeac8a8938fbb357dda90e0c12a4ecc
Dennis Aleynikov <daleynikov@gmail.com>
2018-01-29 02:08:28 -0800

    succesfully spits out random word

--- stochastic.py
+++ stochastic.py
@@ -1,17 +1,30 @@
 import random
 import frequency
+import sys
 
 def main(argv):
     # stop execution if file not specified
-    if len(argv) == 1:
+    if len(argv) == 0:
         raise Exception("No file specified")
 
     h = frequency.histogram(argv[1])
     # call sample
-    # sample()
+    print(sample(h))
 
 def sample(h):
     r = random.random()
+    f = convert_freq(h)
+    i = float(0)
+    for w in f:
+        i = i + w[1]
+        if i > r:
+            return w[0]
+
+def convert_freq(h):
+    freq = []
+    for t in h:
+        freq.append((t[0], frequency.freq(h,t[0])))
+    return freq
 
 
 if __name__ == "__main__":

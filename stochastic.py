275312763c23b81adffdd4d6ad450190b49b8840
Dennis Aleynikov <daleynikov@gmail.com>
2018-01-29 01:45:27 -0800

    make stub for sample function

--- stochastic.py
+++ stochastic.py
@@ -1,5 +1,18 @@
 import random
+import frequency
 
-with open('fish.txt', 'r+') as f:
-    lines = [line.rstrip('\n') for line in f.readlines()]
-    print(random.choice(lines))
+def main(argv):
+    # stop execution if file not specified
+    if len(argv) == 1:
+        raise Exception("No file specified")
+
+    h = frequency.histogram(argv[1])
+    # call sample
+    # sample()
+
+def sample(h):
+    r = random.random()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
